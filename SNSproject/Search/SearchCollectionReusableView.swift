d07386d1816a083f3eee8a8d639c6e7e39c470be
suzzang_g <hyeonnee98@gmail.com>
2023-08-18 23:39:23 +0900

    new group

---
+++ SNSproject/Search/SearchCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCollectionReusableView.swift
+//  SNSproject
+//
+//  Created by 이수현 on 2023/08/18.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+        
+}
